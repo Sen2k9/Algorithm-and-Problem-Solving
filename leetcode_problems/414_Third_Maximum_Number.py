4948b26798ad0c647f97141961831fe7425a61e0
Sen <sajibsen.eee@gmail.com>
2019-12-20 22:47:23 -0600

    #414 Leetcode Problem solved

---
+++ leetcode_problems/414_Third_Maximum_Number.py
@@ -0,0 +1,48 @@
+"""
+Given a non-empty array of integers, return the third maximum number in this array. If it does not exist, return the maximum number. The time complexity must be in O(n).
+
+Example 1:
+
+Input: [3, 2, 1]
+
+Output: 1
+
+Explanation: The third maximum is 1.
+
+Example 2:
+
+Input: [1, 2]
+
+Output: 2
+
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+
+Input: [2, 2, 3, 1]
+
+Output: 1
+
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+
+"""
+
+
+class Solution:
+    def thirdMax(self, nums):
+        arr = list(set(nums))
+        arr.sort(reverse=True)
+        if len(arr) == 3:
+            return min(arr)
+        elif len(arr) < 3:
+            return max(arr)
+        arr.pop(0)
+        arr.pop(0)
+
+        return arr[0]
+
+
+sol = Solution()
+nums = [1, 2]
+print(sol.thirdMax(nums))
