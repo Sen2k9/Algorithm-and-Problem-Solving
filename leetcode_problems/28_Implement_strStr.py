d0cb56bf3201b744beee31235b764bf007d8d7c6
Sen <sajibsen.eee@gmail.com>
2020-01-14 20:04:11 -0600

    #28 Leetcode Problem solved

---
+++ leetcode_problems/28_Implement_strStr.py
@@ -0,0 +1,94 @@
+"""
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+"""
+
+
+class Solution:
+    def strStr(self, haystack, needle):
+        # Solution 1:
+
+        # ned_size = len(needle)
+        # hay_size = len(haystack)
+
+        # if ned_size == 0 or haystack == needle:
+        #     return 0
+        # if ned_size > hay_size:
+        #     return -1
+
+        # for i in range(hay_size - ned_size + 1):
+        #     if haystack[i: i + ned_size] == needle:
+        #         return i
+        # return - 1
+
+        # Solution 2: using built-in function
+        # if needle in haystack:
+        #     return haystack.index(needle)
+        # return - 1
+
+        # Solution 3: using for-else, faster
+
+        # if not needle or needle == haystack:
+        #     return 0
+        # if len(needle) > len(haystack):
+        #     return -1
+
+        # for i in range(len(haystack) - len(needle) + 1):
+        #     for j in range(len(needle)):
+        #         if needle[j] != haystack[i + j]:
+        #             break
+        #     else:
+        #         return i
+
+        # return - 1
+
+        # Solution 4: using try-except block, clean code, O(1) memory
+
+        if needle == "":
+            return 0
+
+        for i in range(len(haystack)):
+
+            try:
+                if haystack[i: i + len(needle)] == needle:
+                    return i
+            except:
+                break
+        return -1
+
+
+sol = Solution()
+haystack = "miss"
+needle = "issip"
+print(sol.strStr(haystack, needle))
+
+"""
+corner case:
+1. needle string greater than haystack
+"aaa"
+"aaaa"
+
+"mississippi"
+"issip"
+"mississippi"
+"pi"
+
+
+"""
