3a8c96dd35fdfcb7ce50850caea3bb6a8f538731
Sen <sajibsen.eee@gmail.com>
2020-02-05 19:49:58 -0600

    #434 Leetcode Problem solved

---
+++ leetcode_problems/434_Number_of_Segments_in_a_String.py
@@ -0,0 +1,20 @@
+"""
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+"""
+
+
+class Solution:
+    def countSegments(self, s: str):
+        return len(s.split())
+
+
+sol = Solution()
+s = "Hello, my name is John"
+print(sol.countSegments(s))
