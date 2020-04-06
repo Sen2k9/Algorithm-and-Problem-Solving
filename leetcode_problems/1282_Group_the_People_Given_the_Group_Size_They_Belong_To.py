5f2a7470f0dcf9845bf098e929cd879f6c9adeb3
Sen <sajibsen.eee@gmail.com>
2020-04-06 11:59:51 -0500

    #1282 Leetcode Problem solved

---
+++ leetcode_problems/1282_Group_the_People_Given_the_Group_Size_They_Belong_To.py
@@ -0,0 +1,54 @@
+"""
+There are n people whose IDs go from 0 to n - 1 and each person belongs exactly to one group. Given the array groupSizes of length n telling the group size each person belongs to, return the groups there are and the people's IDs each group includes.
+
+You can return any solution in any order and the same applies for IDs. Also, it is guaranteed that there exists at least one solution. 
+
+Example 1:
+
+Input: groupSizes = [3,3,3,3,3,1,3]
+Output: [[5],[0,1,2],[3,4,6]]
+Explanation: 
+Other possible solutions are [[2,1,6],[5],[0,4,3]] and [[5],[0,6,2],[4,3,1]].
+
+Example 2:
+
+Input: groupSizes = [2,1,3,3,3,2]
+Output: [[1],[0,5],[2,3,4]]
+
+Constraints:
+
+    groupSizes.length == n
+    1 <= n <= 500
+    1 <= groupSizes[i] <= n
+"""
+
+
+class Solution:
+    def groupThePeople(self, groupSizes):
+        dictionary = {}
+
+        for i, val in enumerate(groupSizes):
+            if val not in dictionary:
+                dictionary[val] = [i]
+            else:
+                dictionary[val].append(i)
+
+        ans = []
+
+        for key, val in dictionary.items():
+            if len(val) == key:
+                ans.append(val)
+            else:
+                itr = 0
+                n = len(val)
+
+                while itr < n:
+                    ans.append(val[itr: itr + key])
+                    itr = itr + key
+
+        return ans
+
+
+sol = Solution()
+groupSizes = [3, 3, 3, 3, 3, 1, 3]
+print(sol.groupThePeople(groupSizes))
