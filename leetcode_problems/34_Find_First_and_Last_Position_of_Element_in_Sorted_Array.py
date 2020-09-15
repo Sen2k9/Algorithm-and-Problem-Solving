62548122d753c8e2204366323afd923650082031
ssen <ssen@re-trans.com>
2020-09-15 17:09:16 -0500

    #34 leetcode problem solved

---
+++ leetcode_problems/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,97 @@
+"""
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+ 
+
+Constraints:
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+nums is a non decreasing array.
+-10^9 <= target <= 10^9
+"""
+class Solution:
+    def searchRange(self, nums, target: int):
+        # corner case
+        if not nums:
+            return [-1, -1]
+        # corner case
+        if target < nums[0] or target > nums[-1]:
+            return [-1, -1]
+
+        startPos = self.left_search(nums, target)
+        #print(startPos)
+
+        if nums[startPos] != target:
+            return [-1, -1]
+            
+        
+        else:
+            endPos = self.right_search(nums, target)
+            #print(endPos)
+            # corner case
+            return [startPos, endPos if nums[endPos] == target else endPos - 1]
+
+
+    def left_search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target or nums[mid] > target:
+                high = mid
+
+            else:
+                low = mid + 1
+
+        return low
+
+    def right_search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target or nums[mid] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return low
+
+sol = Solution()
+nums = [2, 2]; target = 2
+print(sol.searchRange(nums, target))
+
+nums = [2, 2]; target = 4
+print(sol.searchRange(nums, target))
+nums = [1, 2]; target = 1
+print(sol.searchRange(nums, target))
+
+nums = [5,7,7,8,8,10]; target = 8
+print(sol.searchRange(nums, target))
+
+nums = [5,7,7,8,8,10]; target = 6
+print(sol.searchRange(nums, target))
+
+nums = [1]; target = 1
+print(sol.searchRange(nums, target))
+
+nums = [1]; target = 2
+print(sol.searchRange(nums, target))
