95ee189f2f1fc786cd5a3a86bd5be42b959afb59
Sen2k9 <sajibsen.eee@gmail.com>
2020-11-11 19:44:16 -0600

    #216 Leetcode Problem solved

--- leetcode_problems/216_Combination_Sum_III.py
+++ leetcode_problems/216_Combination_Sum_III.py
@@ -44,40 +44,71 @@ Constraints:
 2 <= k <= 9
 1 <= n <= 60
 """
-
+from collections import deque
 
 class Solution:
     def combinationSum3(self, k: int, n: int):
+        # result = []
+        # def backtracking(remaining, combo_list, next_number):
+
+        #     if remaining == 0 and len(combo_list) == k:
+
+        #         # although combo_list is a list we need to cast it to list(combo_list)
+        #         # because list keeps the reference, casting it to list type will remove the reference from the original
+        #         # and will create a new list reference which will be saved in the result list
+        #         # if we don not cast it to list, as at the end of the backtracking function we are popping out every element
+        #         # the result will have three empty list inside of it, because every inner list has same reference for combo_list
+        #         # and we popping out every element from the same reference at the end
+        #         # this is a very pythonic way problem
+        #         result.append(combo_list)
+        #         print(result)
+        #         return
+
+        #     elif remaining < 0 or len(combo_list) > k:
+        #         return
+
+        #     for i in range(next_number, 9):
+        #         combo_list.append(i + 1)
+        #         backtracking(remaining - i - 1, combo_list, i + 1)
+
+        #         # go to next choice
+
+        #         #combo_list.pop()
+
+        # backtracking(n, [], 0)
+
+        # return result
+        # using dfs
+
         result = []
-        def backtracking(remaining, combo_list, next_number):
 
-            if remaining == 0 and len(combo_list) == k:
+        queue = deque()
+        queue.append([9,[],0])
 
-                # although combo_list is a list we need to cast it to list(combo_list)
-                # because list keeps the reference, casting it to list type will remove the reference from the original
-                # and will create a new list reference which will be saved in the result list
-                # if we don not cast it to list, as at the end of the backtracking function we are popping out every element
-                # the result will have three empty list inside of it, because every inner list has same reference for combo_list
-                # and we popping out every element from the same reference at the end
-                # this is a very pythonic way problem
-                result.append(list(combo_list))
-                print(result)
-                return
+        size = 0
+        while queue:
+
+            remaining, combo_list, next_number = queue.popleft()
+
+            # keep track of queue size
+            if len(queue) > size:
+                size = len(queue)
+            
+            if len(combo_list) == k and remaining == 0:
+                result.append(combo_list)
 
             elif remaining < 0 or len(combo_list) > k:
-                return
+                continue
 
-            for i in range(next_number, 9):
-                combo_list.append(i + 1)
-                backtracking(remaining - i - 1, combo_list, i + 1)
+            for i in range(next_number, n):
 
-                # go to next choice
+                queue.append([remaining - i - 1, combo_list + [i+1], i + 1])
 
-                combo_list.pop()
+            #print(queue)
 
-        backtracking(n, [], 0)
+        return result, size
 
-        return result
+
 
 sol = Solution()
 k = 3
