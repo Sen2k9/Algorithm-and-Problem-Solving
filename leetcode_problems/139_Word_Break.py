7badbaaea55a1726d1fa533c0a171916d1e19e96
Sen2k9 <sajibsen.eee@gmail.com>
2020-12-21 10:30:09 -0600

    #139 problem revised

--- leetcode_problems/139_Word_Break.py
+++ leetcode_problems/139_Word_Break.py
@@ -30,25 +30,38 @@ from collections import deque
 class Solution:
     def wordBreak(self, s, wordDict):
         # BFS algorithm
-        word_set = set(wordDict)
-        visited = [0] * len(s)
-        queue = deque()
-        queue.append(0)
-        while queue:
-            start = queue.popleft()
-            if visited[start] == 0:
-                for end in range(start + 1, len(s)+1):
-                    if s[start:end] in word_set:
-                        queue.append(end)
+#        word_set = set(wordDict)
+#        visited = [0] * len(s)
+#        queue = deque()
+#        queue.append(0)
+#        while queue:
+#            start = queue.popleft()
+#            if visited[start] == 0:
+#                for end in range(start + 1, len(s)+1):
+#                    if s[start:end] in word_set:
+#                        queue.append(end)
+#
+#                        if end == len(s):
+#                            return True
+#
+#                visited[start] = 1
+#
+#            print(queue)
+#
+#        return False
 
-                        if end == len(s):
-                            return True
-
-                visited[start] = 1
-
-            print(queue)
-
-        return False
+         dp = [False] * (len(s) + 1)
+         dp[0] = True
+         for i in range(1, len(s) + 1):
+             for j in range(0, i):
+                 if s[j:i] in wordDict and dp[j]:
+                     dp[i] = True
+                     break
+                 if i == len(s):
+                     print(s, s[j:i], i, j, w)
+             print(s, s[:i], i, w)
+             print(dp)
+         return dp[-1]
 
 
 
@@ -57,6 +70,24 @@ s = "cbca"
 w = ["bc","ca"]
 
 print(sol.wordBreak(s, w))
+s = "cars"
+w = ["car","ca","rs"]
+print(sol.wordBreak(s, w))
+
+
+s = "a"
+w = ["a"]
+print(sol.wordBreak(s, w))
+
+s = "catsandog"
+w = ["cats", "dog", "sand", "and", "cat"]
+print(sol.wordBreak(s, w))
+
+s = "leetcode"
+w = ["leet", "code"]
+print(soli.wordBreak(s, w))
+i
+
 
 """
 corner case:
