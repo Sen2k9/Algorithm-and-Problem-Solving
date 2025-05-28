a112092d1d527874e941cca08bd338975e711e38
Sajib Sen <sajibsen.eee@gmail.com>
2025-05-28 10:21:55 -0500

    Add testsuite to Leetcode #33

--- leetcode_problems/33_Search_in_Rotated_Sorted_Array.py
+++ leetcode_problems/33_Search_in_Rotated_Sorted_Array.py
@@ -51,50 +51,73 @@ Constraints:
 
 
 """
+import unittest
+from typing import List
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # find pivot, as smallest element from right boundary
-
-        left = 0
-        right = len(nums) - 1
-
-        while left <= right:
-            mid = left + (right - left) // 2
-            # that means smallest element is on the right side
-            if nums[mid] > nums[-1]:
-                left = mid + 1
-            # either it is the pivot or in the left side
-            # if right go beyond the boundary then the left value is the pivot
-            else:
-                right = mid - 1
         
-        pivot = left
+        def find_pivot(nums):
+            left = 0
+            right = len(nums) - 1
 
-        def binary_search(left_boundary, right_boundary, target):
+            while left <= right:
+                mid = left + (right - left) // 2
 
-            while left_boundary <= right_boundary:
-                mid = left_boundary + (right_boundary - left_boundary) // 2
-
-                if nums[mid] == target:
-                    return mid
-                
-                elif nums[mid] > target:
-                    right_boundary = mid - 1
+                if nums[mid] > nums[-1]: # compare with respect to last element
+                    left = mid + 1 # search right
                 
                 else:
-                    left_boundary = mid + 1
+                    right = mid - 1 # search left
             
-            # otherwise not found
-            return - 1
+            return left
+
+        pivot = find_pivot(nums)
+        #print(pivot)
+
+        def binary_search(arr, target):
+
+            left = 0
+            right = len(arr) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if arr[mid] == target:
+                    return mid
+                
+                elif arr[mid] > target:
+                    right = mid - 1
+                
+                else:
+                    left = mid + 1
+            
+            return -1
+
+        # search left of pivot
+        result = binary_search(nums[:pivot], target)
+        if result > -1:
+            return result
         
-        # find target either in left side of pivot or right side
-        left_search = binary_search(0, left - 1, target)
-        right_search = binary_search(left, len(nums) - 1, target)
-
-        if left_search != -1:
-            return left_search
+        # search right of pivot
+        result = binary_search(nums[pivot:], target)
+        if result > -1:
+            return result + pivot
         
-        return right_search
+        return - 1
 
 
+class TestSuite(unittest.TestCase):
+    
+    def test_search(self):
+        sol = Solution()
+        nums = [4,5,6,7,0,1,2]
+        target = 3
+        
+        self.assertEqual(
+            sol.search(nums, target),
+            -1
+        )
+
+if __name__ == "__main__":
+    unittest.main()
+        
 
