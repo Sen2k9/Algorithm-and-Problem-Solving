c36c5e63e56b3ee3dd3936caff68cb4ba1c96ac8
Sajib Sen <sajibsen.eee@gmail.com>
2025-06-03 21:53:25 -0500

    Leetcode Problem #1209

---
+++ leetcode_problems/1209_Remove_All_Adjacent_Duplicates_in_String_II.py
@@ -0,0 +1,83 @@
+"""
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    2 <= k <= 104
+    s only contains lowercase English letters.
+
+
+"""
+import unittest
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [] # [chars, count]
+
+        for char in s:
+            # add same chars
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+
+            else:
+                stack.append(
+                    [char, 1]
+                )
+            
+            # pop from stack
+            if stack[-1][1] == k:
+                stack.pop()
+        
+        result = ""
+
+        for char, count in stack:
+            result += char * count
+        
+        return result
+
+
+class TestSuit(unittest.TestCase):
+    
+    def test_removeDuplicates(self):
+        s = "deeedbbcccbdaa"
+        k = 3
+        sol = Solution()
+        self.assertEqual(
+            sol.removeDuplicates(
+                s, k
+            ),
+            "aa"
+        )
+        
+
+if __name__ == "__main__":
+    unittest.main()
